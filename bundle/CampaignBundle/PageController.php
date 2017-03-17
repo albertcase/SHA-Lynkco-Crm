60984ac29a5b62b524c2e5edb1aae7cf729d19f9
Demon Zhang <demon.zhang@samesamechina.com>
2017-03-17 18:26:48 +0800

    no message

--- bundle/CampaignBundle/PageController.php
+++ bundle/CampaignBundle/PageController.php
@@ -52,7 +52,7 @@ class PageController extends Controller {
 		$_SESSION['check_code'] = $code;
 		$_SESSION['check_timestamp'] = time();
 		$rs = $SmsAPI->sendMessage($mobile, $code);
-		if ($rs == 0) 
+		if ($rs['result'] == 0) 
 			$data = array('status' => 1, 'msg' => $rs['description']);
 		else
 			$data = array('status' => $rs['result'], 'msg' => $rs['description']);
